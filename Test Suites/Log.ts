56d7c6a82061cb845732ba8ff3fd04cdd4c04296
yl.tey <yl.tey@YaaLian.mk.macrokiosk.com>
2018-04-06 08:58:49 +0800

    commit today

---
+++ Test Suites/Log.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Log</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>7c9bc876-688d-405c-8b06-fc1261f6f5b9</testSuiteGuid>
+   <testCaseLink>
+      <guid>c7092fcb-3fbc-4195-b751-a5a6082a4ea2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TNB_Log</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
